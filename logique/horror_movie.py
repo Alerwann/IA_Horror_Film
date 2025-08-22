f45773b2f26263b523d23e81de5d74cfc83c0fca
Alerwann <alerwann411@gmail.com>
2025-08-22 12:25:01 +0200

    feature : création de la classe principal HorrorMovie

---
+++ logique/horror_movie.py
@@ -0,0 +1,32 @@
+class HorrorMovie:
+    def __init__(self, titre, annee, realisateur, sous_genre, note_sur_10):
+        """
+        Initialisation de la classe film avec vérification des champs
+
+        Args:
+            titre(str), realisateur(str), sous_genre(str):
+                Si 1 de ces champs est vide ou remplis uniquement avec des espaces renvoie None
+                sinon renvoie la donnée
+            annee(int), note_sur_10(int) :
+                si il ne peut être converti en int renvoie None
+                Testé avec try/except
+        """
+        self.titre = titre.strip() if titre and titre.strip() else None
+
+        try:
+            self.annee = int(annee)
+        except (ValueError, TypeError):
+            self.annee = None
+
+        self.realisateur = (
+            realisateur.strip() if realisateur and realisateur.strip() else None
+        )
+
+        self.sous_genre = (
+            sous_genre.strip() if sous_genre and sous_genre.strip() else None
+        )
+
+        try:
+            self.note_sur_10 = int(note_sur_10)
+        except (ValueError, TypeError):
+            self.note_sur_10 = None
