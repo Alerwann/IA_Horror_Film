f74989cc587b52b7df1122a45983ecfee3ec95a6
Alerwann <alerwann411@gmail.com>
2025-08-23 09:53:42 +0200

    feature: creation des fonciton pour obtenir l'initialisation des données

--- logique/analyzer.py
+++ logique/analyzer.py
@@ -5,9 +5,63 @@ from .data_loader import DataLoader
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 class HorrorAnalyser:
+    # ==== INITIALISATION DES DONNEES ====
     def __init__(self):
         """Initialisation des variable"""
-    # note il faudrait que je trouve un moyen pour parcourir le tableau pour trouver que en parcourrant horrorfilm je puisse identifier tous les real, sous genre pour faire un classement
+        self.data_loader=DataLoader()
+        self._total_movie = None
+        self._total_annee = None
+        self._total_realisateur = None
+        self._total_sous_genre = None
+        self._total_note = None
+
+    def get_total_movies(self):
+        if self._total_movies is None:
+            self._total_movies = len(self.data_loader.load_movies())
+        return self._total_movies
+
+    def get_total_information_about(self,theme):
+        informations = len(self.data_loader.get_all_informations(theme) )
+        return informations
+
+    def get_total_note(self):
+        if self._total_note is None :
+           self._total_note = self.get_total_information_about('note_sur_10')
+        return self._total_note
+
+    def get_total_realisateur(self):
+        if self._total_realisateur is None:
+            self._total_realisateur = self.get_total_information_about('realisateur')
+        return self._total_realisateur
+
+    def get_total_sous_genre(self):
+        if self._total_sous_genre is None :
+            self._total_sous_genre = self.get_total_information_about('sous_genre')
+        return self._total_sous_genre
+
+    def get_total_annee(self):
+        if self._total_annee is None : 
+            self._total_annee = self.get_total_information_about('annee')
+        return self._total_annee
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def stat_by_real(self, realisateur):
         """ 
