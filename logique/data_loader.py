657c91e4f773debcaf4407fa223ae9e23d604a8d
Alerwann <alerwann411@gmail.com>
2025-08-23 07:54:52 +0200

    feature : ajout de l'extaction de donnée global par thème

--- logique/data_loader.py
+++ logique/data_loader.py
@@ -12,6 +12,9 @@ from logique.utils import verif_var_int, verif_order_int, str_strip_lower
 
 
 class DataLoader:
+
+    # === INITIALISATION ===
+
     def __init__(self):
         """Initialisation des variables necessaires pour la classe"""
         self.csv_path = CSV_PATH
@@ -54,7 +57,54 @@ class DataLoader:
 
         return self.movies
 
-    # === Gestion des paramètre qui ont besoin d'un retour précis ===
+    # === CORS METHODE ===
+
+    def get_movie_range(self, search_type, var_min, var_max):
+        """
+        Parcours la liste de movies qu'il aura chargé pour trouver l'ensemble des films selon un interval de paramètres demandé
+
+        Args:
+            - search_type(str) : Nom de l'attribut qui est sujet de la recherche
+            - var_min(int): Valeur minimal de l'interval
+            - var_max(int): Valeur maximal de l'interval
+
+        Return:
+            [obj, ... obj] -> tableau d'objet HorrorMovie
+
+        Note :
+                - retourne un tableau vide si aucun film
+
+        """
+        if not self.is_loaded:
+            self.load_movies()
+
+        search_movies = []
+
+        for film in self.movies:
+            film_value = getattr(film, search_type)
+            if film_value is not None and var_min <= film_value <= var_max:
+                search_movies.append(film)
+        return search_movies
+
+    def get_all_informations(self, search):
+        """
+        Retoune une liste de toutes les valeurs uniques d'un attribut
+
+        Ags:
+            search(str) : Nom de l'attribut à extraire (peut être : titre, annee, sous_genre, note_sur_10, realisateur)
+
+        Returns:
+            all_infos[str] : Liste sans doublon des valeurs
+        """
+        all_infos = []
+        if not self.is_loaded:
+            self.load_movies()
+
+        for film in self.movies:
+            search_value = getattr(film, search)
+            if search_value not in all_infos:
+                all_infos.append(search_value)
+        return all_infos
 
     def get_movie(self, search_type, search):
         """
@@ -80,6 +130,30 @@ class DataLoader:
                 search_movies.append(film)
 
         return search_movies
+
+    # ==== SEARCH METHODS - GLOBAL PARAMETER ===
+
+    def get_all_realisateurs(self):
+        """ Fonction permettant de retourner une liste de nom des realisateurs"""
+        all_real = self.get_all_informations('realisateur')
+        return all_real
+
+    def get_all_years(self):
+        """Fonction permettant de retourner une liste unique des années"""
+        all_years= self.get_all_informations('annee')
+        return all_years
+
+    def get_all_note(self):
+        """Fonction permettant de retourner une liste unique des notes"""
+        all_notes = self.get_all_informations('note_sur_10')
+        return all_notes
+
+    def get_all_sous_genre(self):
+        """Fonction permettant de retourner une liste des sous-genre"""
+        all_ssgenre = self.get_all_informations('sous_genre')
+        return all_ssgenre
+    
+    # === SEARCH METHODS - SINGLE PARAMETER ===
 
     def get_movie_by_realisateur(self, real):
         """
@@ -135,50 +209,23 @@ class DataLoader:
         titres = str_strip_lower(titre)
         return self.get_movie("titre", titres)
 
-    # === Gestion des parametètres qui peuvent avoir un interval ===
-
-    def get_movie_range(self, search_type, var_min, var_max):
-        """
-        Parcours la liste de movies qu'il aura chargé pour trouver l'ensemble des films selon un interval de paramètres demandé
-
-        Args:
-            - search_type(str) : Nom de l'attribut qui est sujet de la recherche
-            - var_min(int): Valeur minimal de l'interval
-            - var_max(int): Valeur maximal de l'interval
-
-        Return:
-            [obj, ... obj] -> tableau d'objet HorrorMovie
-
-        Note :
-                - retourne un tableau vide si aucun film
-
-        """
-        if not self.is_loaded:
-            self.load_movies()
-
-        search_movies = []
-
-        for film in self.movies:
-            film_value = getattr(film, search_type)
-            if film_value is not None and var_min <= film_value <= var_max:
-                search_movies.append(film)
-        return search_movies
+    # === SEARCH METHODS - MULTI PARAMETER ===
 
     def get_movie_by_years_range(self, annee_min, annee_max):
         """
-        Parcours l'ensemble des films chargé pour retourné ceux qui sont sortie sur une période ou une année précise
-        Fait appel à get_movie() pour obtenir les résultats
-        Fait appel à verif_var_int() pour vérifié que la donnée peut être traduit en int
+            Parcours l'ensemble des films chargé pour retourné ceux qui sont sortie sur une période ou une année précise
+            Fait appel à get_movie() pour obtenir les résultats
+            Fait appel à verif_var_int() pour vérifié que la donnée peut être traduit en int
 
-        Args:
-            annee_min(int or str), annee_max(int,str): annee donnée par utilisateur pour la plage de recherche.
-            si besoin d'une valeur précise : faire la recherche avec la même valeur pour les 2 args
+            Args:
+                annee_min(int or str), annee_max(int,str): annee donnée par utilisateur pour la plage de recherche.
+                si besoin d'une valeur précise : faire la recherche avec la même valeur pour les 2 args
 
-        Retur:
-            [obj, ... obj] -> list d'objet HorrorMovie
+            Retur:
+                [obj, ... obj] -> list d'objet HorrorMovie
 
-        Note : retourne une liste vide si aucun film trouvé
-        """
+            Note : retourne une liste vide si aucun film trouvé
+            """
         annee_min = verif_var_int(annee_min)
         annee_max = verif_var_int(annee_max)
 
@@ -192,20 +239,20 @@ class DataLoader:
 
     def get_movie_by_note_range(self, note_min, note_max):
         """
-        Fonction qui permet de retourner l'ensembles des films aynt un interval de note ou une note précise
-        Fait appel à get_movie() pour obtenir les résultats
-        Fait appel à verif_var_int() pour vérifié que la donnée peut être traduit en int
+            Fonction qui permet de retourner l'ensembles des films aynt un interval de note ou une note précise
+            Fait appel à get_movie() pour obtenir les résultats
+            Fait appel à verif_var_int() pour vérifié que la donnée peut être traduit en int
 
-        Args:
-            note_min(int or str), note_max(int,str): note donnée par utilisateur pour la plage de recherche.
-            si besoin d'une valeur précise : faire la recherche avec la même valeur pour les 2 args
+            Args:
+                note_min(int or str), note_max(int,str): note donnée par utilisateur pour la plage de recherche.
+                si besoin d'une valeur précise : faire la recherche avec la même valeur pour les 2 args
 
-        Return:
-            [obj, ... obj] -> tableau d'objet HorrorMovie
+            Return:
+                [obj, ... obj] -> tableau d'objet HorrorMovie
 
-        Note :
-            - retourne un tableau vide si aucun film
-            - si l'argument n'est pas un entier, retourne 'la note doit être un nombre valide"
+            Note :
+                - retourne un tableau vide si aucun film
+                - si l'argument n'est pas un entier, retourne 'la note doit être un nombre valide"
         """
         note_min = verif_var_int(note_min)
         note_max = verif_var_int(note_max)
@@ -217,7 +264,6 @@ class DataLoader:
 
 if __name__ == "__main__":
     loader = DataLoader()
-
 
     movie_year = loader.get_movie_by_years_range(2005, 200)
     print(f"{len(movie_year)}")
