562d5b99bea3e97e0694fabd1906ad83fe56d709
Alerwann <alerwann411@gmail.com>
2025-08-23 14:47:00 +0200

    feature : ajout de variable de init

--- logique/recommender.py
+++ logique/recommender.py
@@ -1,8 +1,16 @@
-
+import sys
+import os
 from .data_loader import DataLoader
 from .analyzer import HorrorAnalyser
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 
 class HorrorRecommender:
     def __init__(self):
         """Initialisation des données"""
         self.analyzer = HorrorAnalyser
+        self._best_realisateur = None
+        self._best_sous_genre = None
+        self._best_periode = None
+        self._best_note = None
